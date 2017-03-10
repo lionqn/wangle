b832e09fc0e6ec499b3770ade1d75505ffb49b06
lion <1099878880@qq.com>
2017-03-11 00:47:17 +0800

    标签栏加载数据成功

--- wangyi/wangyi/Classes/Home/ViewController/HomeViewController.m
+++ wangyi/wangyi/Classes/Home/ViewController/HomeViewController.m
@@ -8,8 +8,9 @@
 
 #import "HomeViewController.h"
 #import "ChannelModel.h"
+#import "ChannelLabel.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 
 // 频道视图
 @property (weak, nonatomic) IBOutlet UIScrollView *channelScrollView;
@@ -20,6 +21,10 @@
 // 布局对象
 @property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
 
+// 数据源
+@property (nonatomic, strong) NSArray *channelModelArray;
+
+
 @end
 
 @implementation HomeViewController
@@ -28,20 +33,56 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
     [self requestChannelData];
 }
 
 
 -(void)requestChannelData{
     
-    NSArray *channelModelArray = [ChannelModel getChannelModelArray];
+    self.channelModelArray = [ChannelModel getChannelModelArray];
     
-//    NSLog(@"tList对应的数组%@",channelModelArray);
-    for (ChannelModel *model in channelModelArray) {
+
+//    for (ChannelModel *model in channelModelArray) {
+//        
+//        NSLog(@"%@",model);
+//    }
+    
+    // 遍历频道模型数组,创建对应频道的label
+    // 频道的大小
+    CGFloat labelW = 80;
+    CGFloat labelH = 44;
+    for (int i = 0; i < self.channelModelArray.count; i++) {
         
-        NSLog(@"%@",model);
+        // 获取对应的模型数据
+        ChannelModel *model = self.channelModelArray[i];
+        
+        // 创建label
+        ChannelLabel *channelLabel = [[ChannelLabel alloc] initWithFrame:CGRectMake(i * labelW, 0, labelW, labelH)];
+        channelLabel.text = model.tname;
+        
+        channelLabel.font = [UIFont systemFontOfSize:15];
+        channelLabel.textAlignment = NSTextAlignmentCenter;
+        channelLabel.backgroundColor = [UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:1];
+        [self.channelScrollView addSubview:channelLabel];
     }
     
+    
+    // 设置scrollView的滑动大小
+    self.channelScrollView.contentSize = CGSizeMake(labelW * self.channelModelArray.count, 44);
+    
+    self.channelScrollView.showsVerticalScrollIndicator = NO;
+    self.channelScrollView.showsHorizontalScrollIndicator = NO;
+    
+    
+}
+
+
+// 创建新闻视图
+-(void)setupNewCollectionView{
+    
+    // 设置数据源代理
     
 }
 
