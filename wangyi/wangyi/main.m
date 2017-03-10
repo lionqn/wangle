4544ed2e9008665b711c59124a8caa619559f062
lion <1099878880@qq.com>
2017-03-10 19:07:19 +0800

    初始化项目

---
+++ wangyi/wangyi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wangyi
+//
+//  Created by danny on 17/3/10.
+//  Copyright © 2017年 qinningning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
